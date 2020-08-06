2bec0644c6f78d0b9c266479813f1e4f671e4d4c
imansarahaji <68403341+imansarahaji@users.noreply.github.com>
2020-08-06 14:32:47 -0700

    Create form-to-email.php

---
+++ form-to-email.php
@@ -0,0 +1,52 @@
+<?php
+	$email_from = 'imansarahaji@gmail.com';
+
+	$email_subject = "New Form submission";
+
+	$email_body = "You have received a new message from the user $name.\n".
+                            "Here is the message:\n $message".
+?>
+
+<?php
+
+  $to = "yourname@yourwebsite.com";
+
+  $headers = "From: $email_from \r\n";
+
+  $headers .= "Reply-To: $visitor_email \r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+
+ ?>
+ 
+ <?php
+function IsInjected($str)
+{
+    $injections = array('(\n+)',
+           '(\r+)',
+           '(\t+)',
+           '(%0A+)',
+           '(%0D+)',
+           '(%08+)',
+           '(%09+)'
+           );
+               
+    $inject = join('|', $injections);
+    $inject = "/$inject/i";
+    
+    if(preg_match($inject,$str))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+}
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+?>
